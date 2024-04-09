b4a3824cb024458449a5e2534b37334f2d44e221
Ankush Kumar <115498007+Ankush12217540@users.noreply.github.com>
2024-04-09 23:54:00 +0530

    index.js

--- JAVASCRIPT   in details/APIs/client-side-storage/web-storage/index.js
+++ JAVASCRIPT   in details/APIs/client-side-storage/web-storage/index.js
@@ -3,19 +3,19 @@ const rememberDiv = document.querySelector('.remember');
 const forgetDiv = document.querySelector('.forget');
 const form = document.querySelector('form');
 const nameInput = document.querySelector('#entername');
-const submitBtn = document.querySelector('#submitname');
+const submitBtn = document.querySelector('#submitname'); 
 const forgetBtn = document.querySelector('#forgetname');
 
-const h1 = document.querySelector('h1');
+const h1 = document.querySelector('h1');  
 const personalGreeting = document.querySelector('.personal-greeting');
 
 // Stop the form from submitting when a button is pressed
 form.addEventListener('submit', e => e.preventDefault());
-
+ 
 // run function when the 'Say hello' button is clicked
 submitBtn.addEventListener('click', () => {
-  // store the entered name in web storage
-  localStorage.setItem('name', nameInput.value);
+  // store the entered name in web storage 
+  localStorage.setItem('name', nameInput.value); 
   // run nameDisplayCheck() to sort out displaying the personalized greetings and updating the form display
   nameDisplayCheck();
 });
