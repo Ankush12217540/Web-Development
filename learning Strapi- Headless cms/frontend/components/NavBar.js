c0df53237a04d80c5d2d0514cafc1ba059425b28
Ankush Kumar <115498007+Ankush12217540@users.noreply.github.com>
2024-07-30 23:59:53 +0530

    NavBar.js

--- learning Strapi- Headless cms/frontend/components/NavBar.js
+++ learning Strapi- Headless cms/frontend/components/NavBar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 
 const NavBar = ({cart}) => {
-  return (
+  return ( 
     <div><header className="text-gray-600 body-font">
     <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
     <Link href="/"><a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
